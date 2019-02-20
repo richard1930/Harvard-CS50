3af375d6bb2eed7483848a96aa02d529faa680ea
Ricardo <richard1930@gmail.com>
2019-02-20 08:48:15 +0000

    start python

---
+++ hello.py
@@ -0,0 +1,14 @@
+x = 28
+
+if x > 0:
+    print("x is positive")
+elif x < 0:
+    print("x is negative")
+else:
+    print("x is zero")
+
+name = "alice"
+coordinates = (10.0, 20.0)
+names = ["alice", "bob", "charlie"]
+
+print type(name)
